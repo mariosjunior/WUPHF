fe0e398c346d6448b9074f3c466dbba684b7486b
Mario Jr <54999734+mariosjunior@users.noreply.github.com>
2024-04-19 09:55:56 -0300

    fix: styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,15 +2,40 @@ import "../App.css";
 import Heading from "./Heading.jsx";
 import Footer from "./Footer.jsx";
 import Note from "./Note.jsx";
-import notes from "../notes.js";
+import CreateArea from "./CreateArea.jsx";
+import { useState } from "react";
 
 function App() {
+  const [notes, setNotes] = useState([]);
+
+  function addNote(newNote) {
+    setNotes((prevNotes) => {
+      return [...prevNotes, newNote];
+    });
+  }
+
+  function deleteNote(id) {
+    console.log(id);
+    setNotes((prevNotes) => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      });
+    });
+  }
+
   return (
     <>
       <div>
         <Heading></Heading>
-        {notes.map((note) => (
-          <Note key={note.id} title={note.title} content={note.content}></Note>
+        <CreateArea onAdd={addNote}></CreateArea>
+        {notes.map((note, index) => (
+          <Note
+            key={index}
+            id={index}
+            title={note.title}
+            content={note.content}
+            onDelete={deleteNote}
+          ></Note>
         ))}
         <Footer></Footer>
       </div>
